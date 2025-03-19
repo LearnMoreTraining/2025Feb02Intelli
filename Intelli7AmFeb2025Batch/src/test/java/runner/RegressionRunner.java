46bed067b46991eb2962a3cd5f123656aec91f85
LearnMoreTraining <anaravinthh@outlook.com>
2025-03-19 08:33:41 +0530

    plugin

---
+++ Intelli7AmFeb2025Batch/src/test/java/runner/RegressionRunner.java
@@ -0,0 +1,20 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+        features = {"src/test/resources/testdesign"},
+        glue = {"stepdefinition","hooks"},
+        tags = "@sc01",
+        plugin = {"pretty","html:target/report/report.html",
+                "json:target/report/report.json",
+                "junit:target/report/report.xml"}
+)
+
+public class RegressionRunner {
+
+}
